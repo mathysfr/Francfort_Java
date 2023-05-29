099cdd3cc830f910b22002a7ef8f0d93cb8002eb
mathysfr <matprofrancfort@gmail.com>
2023-05-29 16:22:47 +0200

    first commit

---
+++ 3IRC_POO_FilRouge_Atelier3/atelier3/model/GameModel.java
@@ -0,0 +1,28 @@
+package atelier3.model;
+
+public class GameModel {
+    int scorePlayer1;
+    int scorePlayer2;
+
+    public void displayScores() {
+        System.out.println("Scores:");
+        System.out.println("Player 1: " + scorePlayer1);
+        System.out.println("Player 2: " + scorePlayer2);
+    }
+    
+    public boolean isGameOver() {
+        // Définissez ici le score maximal pour la victoire
+        int maxScore = 20;
+        
+        return scorePlayer1 >= maxScore || scorePlayer2 >= maxScore;
+    }
+
+    public void displayWinner() {
+        if (scorePlayer1 > scorePlayer2) {
+            System.out.println("Fin de la partie ! Les noirs ont gagnés !");
+        } else if (scorePlayer2 > scorePlayer1) {
+            System.out.println("Fin de la partie ! Les blancs ont gagnés !");
+        } 
+    }
+    
+}
